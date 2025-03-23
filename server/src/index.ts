a42d1690a2801f0f01a1d516ad97433df4bc5d77
harshit391 <harshitsingla1761@gmail.com>
2025-03-24 00:14:08 +0530

    [Backend] | CORS Updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,13 @@ const app = express();
 
 const prisma = new PrismaClient();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
 
 app.use(express.json());
 
